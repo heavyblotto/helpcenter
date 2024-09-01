760183a0c197ece6512b72c7d0ae2e9846ab6770
JK <jeremykrane@protonmail.com>
2024-08-31 23:22:28 -0400

    fixed deploy config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://heavyblotto.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/heavyblotto/',
+  baseUrl: '/helpcenter/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
