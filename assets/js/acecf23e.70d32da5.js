180a497972b3dafd9b40be43475393fe98e3dca5
JK <jeremykrane@protonmail.com>
2024-08-31 23:21:21 -0400

    Deploy website - based on b892ef835a9563ad27d20d7733fa50ffe3718c73

---
+++ assets/js/acecf23e.70d32da5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhelpcenter=self.webpackChunkhelpcenter||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/heavyblotto/blog","blogTitle":"Blog","authorsListPath":"/heavyblotto/blog/authors"}')}}]);
---
+++ assets/js/f79f2fba.778b0700.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhelpcenter=self.webpackChunkhelpcenter||[]).push([[5080],{6739:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/heavyblotto/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
